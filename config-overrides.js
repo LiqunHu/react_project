2ea428422c922eff3aa23242d4d6251b2950eba3
胡力群 <huliquns@126.com>
2021-08-11 13:59:27 +0800

    增加代理以及请求封装

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,29 @@
-const { override, fixBabelImports, addWebpackAlias } = require('customize-cra');
-const path = require("path");
+const { override, fixBabelImports, addWebpackAlias, overrideDevServer } = require('customize-cra')
+const path = require('path')
 
-const pkg = override(
+const addProxy = () => (configFunction) => {
+  configFunction.proxy = {
+    '/v1/api': {
+      target: 'https://test.shipformula.com', // 接口的域名
+      secure: true, // 如果是https接口，需要配置这个参数
+      changeOrigin: true // 如果接口跨域，需要进行这个参数配置
+    }
+  }
+
+  return configFunction
+}
+
+module.exports = {
+  webpack: override(
     fixBabelImports('import', {
-        libraryName: 'antd',
-        style: 'css',
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: 'css'
     }),
     addWebpackAlias({
-        // add your alias
-        ['@']: path.resolve(__dirname, './src')
+      // add your alias
+      ['@']: path.resolve(__dirname, './src')
     })
-)
-module.exports = pkg
+  ),
+  devServer: overrideDevServer(addProxy())
+}
--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Button } from 'antd'
+import request from '@/utils/http'
 
 function Login() {
+  const [logindata, setLogindata] = useState({})
+  const onLogin = async () => {
+    try {
+      let response = await request.post('/v1/api/node/auth/signin', {
+        identify_code: 'Sxc6dbdIeNEMnj1nCF6ZaA==',
+        login_type: 'ADMIN',
+        magic_no: '1b4be3e317826d0c9ca2e8b54bd04710',
+        username: '18698729476'
+      })
+      console.log(response)
+      setLogindata(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <div>
-      <h3>LOGIN</h3>
+      <Button type="primary" onClick={onLogin}>
+        Primary Button
+      </Button>
+      <div>{JSON.stringify(logindata)}</div>
     </div>
   )
 }
